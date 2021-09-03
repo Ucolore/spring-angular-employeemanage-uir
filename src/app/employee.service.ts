68a0485650aba0f96bdbbd7cc3bd30cd7583a74d
Joaquim Ucolore <joaquim.ucolore@mozabanco.co.mz>
2021-09-03 15:52:11 +0200

    Service requests

---
+++ src/app/employee.service.ts
@@ -0,0 +1,37 @@
+import { Observable } from "rxjs";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Employee} from "./employee";
+
+@Injectable({
+  providedIn: "root"
+})
+export class EmployeeService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) {}
+
+  public getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${id}`);
+  }
+
+  public findEmployeeById(employeeId: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiServerUrl}/employee/findEmployeeById/${employeeId}`);
+  }
+
+  public findEmployeeByCode(employeeCode: string): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiServerUrl}/employee/findEmployeeByCode/${employeeCode}`);
+  }
+}
