c6212e466e815a6ae43c98357ec01bac8ac45177
Joaquim Ucolore <joaquim.ucolore@mozabanco.co.mz>
2021-09-08 16:05:32 +0200

    define some imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {EmployeeService} from "./employee.service";
 
 @NgModule({
   declarations: [
